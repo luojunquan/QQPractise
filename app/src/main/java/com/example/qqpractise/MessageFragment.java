828515c9bbdb49c5d491a850406bd7d88cc45d75
luojunquan.github.io <ljq906416@gmail.com>
2016-07-24 17:08:34 +0800

    仿QQ首页，关于低栏的页面

---
+++ app/src/main/java/com/example/qqpractise/MessageFragment.java
@@ -0,0 +1,17 @@
+package com.example.qqpractise;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+public class MessageFragment extends Fragment {
+
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		View messageLayout = inflater.inflate(R.layout.message_layout,
+				container, false);
+		return messageLayout;
+	}
+
+}
